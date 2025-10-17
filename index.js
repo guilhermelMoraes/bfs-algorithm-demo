4b4ba9946f7fba9f2080fe170cbe44fe4b691500
guilhermelMoraes <guilherme.lmoraes.devel@gmail.com>
2025-10-17 16:51:24 -0300

    refactor: renaming stuff and polishing code

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
-class Game {
+class BreadthFirstSearchAlgorithmDemo {
   #board = document.getElementById('board');
   #destinationSquare = null;
   #sprite;
   #squareProps = [
     [
-      { weight: null, enabled: false, index: 0 },
-      { weight: null, enabled: false, index: 1 },
-      { weight: null, enabled: false, index: 2 },
-      { weight: null, enabled: false, index: 3 },
-      { weight: null, enabled: false, index: 4 },
-      { weight: null, enabled: true, index: 5 },
-      { weight: null, enabled: false, index: 6 },
-      { weight: null, enabled: false, index: 7 },
+      { enabled: false, index: 0 },
+      { enabled: false, index: 1 },
+      { enabled: false, index: 2 },
+      { enabled: false, index: 3 },
+      { enabled: false, index: 4 },
+      { enabled: true, index: 5 },
+      { enabled: false, index: 6 },
+      { enabled: false, index: 7 },
     ],
     [
-      { weight: null, enabled: false, index: 8 },
-      { weight: null, enabled: true, index: 9 },
-      { weight: null, enabled: true, index: 10 },
-      { weight: null, enabled: true, index: 11 },
-      { weight: null, enabled: true, index: 12 },
-      { weight: null, enabled: true, index: 13 },
-      { weight: null, enabled: true, index: 14 },
-      { weight: null, enabled: false, index: 15 },
+      { enabled: false, index: 8 },
+      { enabled: true, index: 9 },
+      { enabled: true, index: 10 },
+      { enabled: true, index: 11 },
+      { enabled: true, index: 12 },
+      { enabled: true, index: 13 },
+      { enabled: true, index: 14 },
+      { enabled: false, index: 15 },
     ],
     [
-      { weight: null, enabled: false, index: 16 },
-      { weight: null, enabled: true, index: 17 },
-      { weight: null, enabled: false, index: 18 },
-      { weight: null, enabled: false, index: 19 },
-      { weight: null, enabled: false, index: 20 },
-      { weight: null, enabled: false, index: 21 },
-      { weight: null, enabled: true, index: 22 },
-      { weight: null, enabled: false, index: 23 },
+      { enabled: false, index: 16 },
+      { enabled: true, index: 17 },
+      { enabled: false, index: 18 },
+      { enabled: false, index: 19 },
+      { enabled: false, index: 20 },
+      { enabled: false, index: 21 },
+      { enabled: true, index: 22 },
+      { enabled: false, index: 23 },
     ],
     [
-      { weight: null, enabled: false, index: 24 },
-      { weight: null, enabled: true, index: 25 },
-      { weight: null, enabled: false, index: 26 },
-      { weight: null, enabled: true, index: 27 },
-      { weight: null, enabled: false, index: 28 },
-      { weight: null, enabled: true, index: 29 },
-      { weight: null, enabled: true, index: 30 },
-      { weight: null, enabled: false, index: 31 },
+      { enabled: false, index: 24 },
+      { enabled: true, index: 25 },
+      { enabled: false, index: 26 },
+      { enabled: true, index: 27 },
+      { enabled: false, index: 28 },
+      { enabled: true, index: 29 },
+      { enabled: true, index: 30 },
+      { enabled: false, index: 31 },
     ],
     [
-      { weight: null, enabled: false, index: 32 },
-      { weight: null, enabled: true, index: 33 },
-      { weight: null, enabled: false, index: 34 },
-      { weight: null, enabled: true, index: 35 },
-      { weight: null, enabled: false, index: 36 },
-      { weight: null, enabled: false, index: 37 },
-      { weight: null, enabled: true, index: 38 },
-      { weight: null, enabled: false, index: 39 },
+      { enabled: false, index: 32 },
+      { enabled: true, index: 33 },
+      { enabled: false, index: 34 },
+      { enabled: true, index: 35 },
+      { enabled: false, index: 36 },
+      { enabled: false, index: 37 },
+      { enabled: true, index: 38 },
+      { enabled: false, index: 39 },
     ],
     [
-      { weight: null, enabled: false, index: 40 },
-      { weight: null, enabled: true, index: 41 },
-      { weight: null, enabled: false, index: 42 },
-      { weight: null, enabled: true, index: 43 },
-      { weight: null, enabled: false, index: 44 },
-      { weight: null, enabled: true, index: 45 },
-      { weight: null, enabled: true, index: 46 },
-      { weight: null, enabled: false, index: 47 },
+      { enabled: false, index: 40 },
+      { enabled: true, index: 41 },
+      { enabled: false, index: 42 },
+      { enabled: true, index: 43 },
+      { enabled: false, index: 44 },
+      { enabled: true, index: 45 },
+      { enabled: true, index: 46 },
+      { enabled: false, index: 47 },
     ],
     [
-      { weight: null, enabled: false, index: 48 },
-      { weight: null, enabled: true, index: 49 },
-      { weight: null, enabled: true, index: 50 },
-      { weight: null, enabled: true, index: 51 },
-      { weight: null, enabled: true, index: 52 },
-      { weight: null, enabled: true, index: 53 },
-      { weight: null, enabled: true, index: 54 },
-      { weight: null, enabled: false, index: 55 },
+      { enabled: false, index: 48 },
+      { enabled: true, index: 49 },
+      { enabled: true, index: 50 },
+      { enabled: true, index: 51 },
+      { enabled: true, index: 52 },
+      { enabled: true, index: 53 },
+      { enabled: true, index: 54 },
+      { enabled: false, index: 55 },
     ],
     [
-      { weight: null, enabled: false, index: 56 },
-      { weight: null, enabled: false, index: 57 },
-      { weight: null, enabled: false, index: 58 },
-      { weight: null, enabled: false, index: 59 },
-      { weight: null, enabled: false, index: 60 },
-      { weight: null, enabled: false, index: 61 },
-      { weight: null, enabled: false, index: 62 },
-      { weight: null, enabled: false, index: 63 },
+      { enabled: false, index: 56 },
+      { enabled: false, index: 57 },
+      { enabled: false, index: 58 },
+      { enabled: false, index: 59 },
+      { enabled: false, index: 60 },
+      { enabled: false, index: 61 },
+      { enabled: false, index: 62 },
+      { enabled: false, index: 63 },
     ],
   ];
   #movementInterval = null;
@@ -101,7 +101,6 @@ class Game {
       this.#movementAudio.preload = 'auto';
       this.#movementAudio.volume = 1;
     }
-
 
     loadAudio();
     loadSprite();
@@ -211,13 +210,13 @@ class Game {
   
   #createBoard() {
     this.#squareProps.forEach((column) => {
-      column.forEach(({ enabled, index, weight }) => {
+      column.forEach(({ enabled, index }) => {
         const square = document.createElement('div');
         square.classList.add('square');
         square.id = `square-${index}`;
-        square.dataset.weight = weight;
         
         if (enabled) {
+          square.dataset.weight = '';
           square.classList.add('square--enabled');
           square.addEventListener('click', this.markDestination.bind(this));
         }
@@ -239,5 +238,5 @@ class Game {
   }
 }
 
-const match = new Game();
-match.start();
+const demo = new BreadthFirstSearchAlgorithmDemo();
+demo.start();
